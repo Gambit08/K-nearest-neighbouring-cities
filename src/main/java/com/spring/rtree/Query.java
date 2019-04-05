735717f38411e1eebea0517c0abcea8f18cad993
Gurminder Parmar <gurminder@ajnoha.local>
2019-04-04 22:57:26 -0300

    adding test class

--- src/main/java/com/spring/rtree/Query.java
+++ src/main/java/com/spring/rtree/Query.java
@@ -1,7 +1,9 @@
 package com.spring.rtree;
 
+import java.util.Iterator;
 import java.util.List;
 
+import com.github.davidmoten.rtree.*;
 import rx.Observable;
 import rx.functions.Func1;
 
@@ -9,26 +11,57 @@ import com.github.davidmoten.grumpy.core.Position;
 import com.github.davidmoten.rtree.geometry.Point;
 import com.github.davidmoten.rtree.geometry.Rectangle;
 import com.github.davidmoten.rtree.geometry.Geometries;
-import com.github.davidmoten.rtree.RTree;
-import com.github.davidmoten.rtree.Entry;
 
 public class Query {
 
     private static final Point sydney = Geometries.point(151.2094, -33.86);
     private static final Point canberra = Geometries.point(149.1244, -35.3075);
     private static final Point brisbane = Geometries.point(153.0278, -27.4679);
+    private  static RTree<String, Point> tree = RTree.star().create();
+
 
     public static void main(String args[]){
 
-        RTree<String, Point> tree = RTree.star().create();
         tree = tree.add("Sydney", sydney);
         tree = tree.add("Brisbane", brisbane);
+        tree = tree.add("Canberra",canberra);
+        Node node = tree.root().get();
+        Query query = new Query();
+        query.fetchPlace(node,"Sydney");
         // Now search for all locations within 300km of Canberra
         final double distanceKm =300;
         //Observable<Entry<String,Point>> results = tree.entries();
         List<Entry<String,Point>> results = search(tree,canberra,distanceKm).toList().toBlocking().single();
         System.out.println(results.size());
         System.out.println(results.get(0));
+    }
+
+    public Point fetchPlace(Node node, String search){
+
+        Point point=null;
+
+        if (node instanceof NonLeaf) {
+            NonLeaf<String, Point> n = (NonLeaf)node;
+
+            for(int i = 0; i < n.count(); ++i) {
+                Node<String, Point> child = n.child(i);
+                this.fetchPlace(child, search);
+            }
+        } else {
+
+            Leaf<String, Point> leaf = (Leaf)node;
+            Iterator var8 = leaf.entries().iterator();
+
+            while(var8.hasNext()) {
+                Entry<String, Point> entry = (Entry)var8.next();
+                if(entry.value().equalsIgnoreCase(search)){
+                    point = entry.geometry();
+                }
+            }
+        }
+        //System.out.println(point.toString());
+        return point;
+
     }
 
     public static <T> Observable<Entry<T, Point>> search(RTree<T, Point> tree, Point lonLat,
