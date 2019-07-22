41a4d2e2c2b05d6ef333044330fd54d044c75f2d
gurminder <gurminder95@gmail.com>
2019-07-22 00:07:08 -0300

    updating API file

--- src/main/java/com/spring/rtree/controller/AppController.java
+++ src/main/java/com/spring/rtree/controller/AppController.java
@@ -1,13 +1,15 @@
 package com.spring.rtree.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.davidmoten.grumpy.core.Position;
 import com.github.davidmoten.rtree.*;
 import com.github.davidmoten.rtree.geometry.Geometries;
 import com.github.davidmoten.rtree.geometry.Point;
 import com.github.davidmoten.rtree.geometry.Rectangle;
+import com.opencsv.CSVReader;
 import com.spring.rtree.models.Coordinate;
-import com.spring.rtree.models.Query;
 import org.springframework.ui.Model;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -15,8 +17,16 @@ import org.springframework.stereotype.Controller;
 import rx.Observable;
 import rx.functions.Func1;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import com.spring.rtree.models.Query;
+import com.spring.rtree.models.Result;
+
 
 @Controller
 public class AppController {
@@ -24,6 +34,7 @@ public class AppController {
     // inject via application.properties
     @Value("${welcome.message:test}")
     private String message = "Hello World";
+    private static CSVReader reader = null;
 
     private static final Point sydney = Geometries.point(151.2094, -33.86);// longitude, latitude
     private static final Point canberra = Geometries.point(149.1244, -35.3075);
@@ -34,6 +45,43 @@ public class AppController {
         //adding some tree nodes
         tree = tree.add("Sydney", sydney);
         tree = tree.add("Brisbane", brisbane);
+    }
+
+    @RequestMapping(value = "/loadscript")
+    public String loadScript(){
+        System.out.println("********************************LOADING DATA****************************************");
+        try {
+            int count =0;
+            File file = ResourceUtils.getFile("classpath:worldcities.csv");
+            reader = new CSVReader(new FileReader(file));
+            String [] nextLine;
+            //Read one line at a time
+            long startTime = System.nanoTime();
+            reader.readNext(); //skip the first line
+            while ((nextLine = reader.readNext()) != null)
+            {
+                if(count<=5){
+                    System.out.println(nextLine[0]+", "+nextLine[1]+", "+nextLine[2]+", "+nextLine[3]+", "+nextLine[4]+", "+nextLine[5]+", "+nextLine[6]);
+                }
+                tree=tree.add(nextLine[1],Geometries.point(Double.parseDouble(nextLine[3].trim()),Double.parseDouble(nextLine[2].trim())));
+                count++;
+            }
+
+            System.out.println(tree.entries().toList().toBlocking().single().size()+" Number of entries Added Successfully");
+            long stopTime = System.nanoTime();
+            System.out.println("Total time taken to load dataset: "+(stopTime - startTime));
+        }
+        catch(Exception e){
+            System.out.println("Cause: "+e.getCause());
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "Landing";
     }
 
 
@@ -62,14 +110,27 @@ public class AppController {
     }
 
     @RequestMapping(value = "/service/rtree/query", method = RequestMethod.POST)
-    public String location(@ModelAttribute Query query){
+    public String location(@ModelAttribute Query query,Model model){
         Node node = tree.root().get();
         String location = query.getLocation();
         long maxDistance = query.getDistance();
-        Point geometryPoint = fetchPlace(node,location);
+        float latitude = query.getLatitude();
+        float longitude = query.getLongitude();
+        Point geometryPoint = Geometries.point(longitude,latitude);
         List<Entry<String,Point>> results = search(tree,geometryPoint,maxDistance).toList().toBlocking().single();
-        printResults(results);
-        return "Query";
+        ObjectMapper mapper = new ObjectMapper();
+        List<Result> listOfResults = addToList(results);
+        model.addAttribute("results", listOfResults);
+        return "Result";
+    }
+
+    public List<Result> addToList(List<Entry<String,Point>> results){
+
+        List<Result> listOfResults =  new ArrayList<Result>();
+        for(Entry<String,Point> result:results){
+            listOfResults.add(new Result(result.value(),result.geometry().y(),result.geometry().x()));
+        }
+        return listOfResults;
     }
 
     public void printResults(List<Entry<String,Point>> results){
@@ -82,25 +143,30 @@ public class AppController {
     public Point fetchPlace(Node node, String search){
 
         Point point=null;
+        try {
+            if (node instanceof NonLeaf) {
+                NonLeaf<String, Point> n = (NonLeaf) node;
 
-        if (node instanceof NonLeaf) {
-            NonLeaf<String, Point> n = (NonLeaf)node;
+                for (int i = 0; i < n.count(); ++i) {
+                    Node<String, Point> child = n.child(i);
+                    this.fetchPlace(child, search);
+                }
+            } else {
 
-            for(int i = 0; i < n.count(); ++i) {
-                Node<String, Point> child = n.child(i);
-                this.fetchPlace(child, search);
-            }
-        } else {
+                Leaf<String, Point> leaf = (Leaf) node;
+                Iterator var8 = leaf.entries().iterator();
 
-            Leaf<String, Point> leaf = (Leaf) node;
-            Iterator var8 = leaf.entries().iterator();
-
-            while (var8.hasNext()) {
-                Entry<String, Point> entry = (Entry) var8.next();
-                if (entry.value().equalsIgnoreCase(search)) {
-                    point = entry.geometry();
+                while (var8.hasNext()) {
+                    Entry<String, Point> entry = (Entry) var8.next();
+                    if (entry.value().equalsIgnoreCase(search)) {
+                        point = entry.geometry();
+                    }
                 }
             }
+        }
+        catch(Exception e){
+            System.out.println("Exception thrown in fetchPlace function");
+            System.out.println(e.getCause());
         }
         return point;
     }
