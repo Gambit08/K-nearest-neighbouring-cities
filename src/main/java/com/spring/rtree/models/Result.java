701bf86a1f2d4cdb8bb0ede9266e6c9cba2ad1d3
gurminder <gurminder95@gmail.com>
2019-07-22 00:16:22 -0300

    adding Result model

---
+++ src/main/java/com/spring/rtree/models/Result.java
@@ -0,0 +1,43 @@
+package com.spring.rtree.models;
+
+
+public class Result {
+
+    private String location;
+    private float latitude;
+    private float longitude;
+
+    public Result(){
+
+    }
+
+    public Result(String location, float latitude, float longitude) {
+        this.location = location;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+}
